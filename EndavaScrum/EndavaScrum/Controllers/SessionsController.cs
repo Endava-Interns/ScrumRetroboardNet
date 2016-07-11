7aa5d3ea1729b223b667a146d7a3d85e3bbffcfd
andrej-naumovski <a.naumovski996@yahoo.com>
2016-07-11 17:15:32 +0200

    Fix merge conflict

--- EndavaScrum/EndavaScrum/Controllers/SessionsController.cs
+++ EndavaScrum/EndavaScrum/Controllers/SessionsController.cs
@@ -9,7 +9,6 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using EndavaScrum.Models;
-<<<<<<< HEAD
 using System.Text;
 
 namespace EndavaScrum.Controllers {
@@ -18,34 +17,14 @@ namespace EndavaScrum.Controllers {
 
         // GET: api/Sessions
         public IQueryable<Session> GetSessions() {
-=======
-
-namespace EndavaScrum.Controllers
-{
-    public class SessionsController : ApiController
-    {
-        private DbEntities db = new DbEntities();
-
-        // GET: api/Sessions
-        public IQueryable<Session> GetSessions()
-        {
->>>>>>> master
             return db.Sessions;
         }
 
         // GET: api/Sessions/5
         [ResponseType(typeof(Session))]
-<<<<<<< HEAD
         public IHttpActionResult GetSession(string id) {
             Session session = db.Sessions.Find(id);
             if (session == null) {
-=======
-        public IHttpActionResult GetSession(string id)
-        {
-            Session session = db.Sessions.Find(id);
-            if (session == null)
-            {
->>>>>>> master
                 return NotFound();
             }
 
@@ -54,94 +33,44 @@ namespace EndavaScrum.Controllers
 
         // PUT: api/Sessions/5
         [ResponseType(typeof(void))]
-<<<<<<< HEAD
         public IHttpActionResult PutSession(string id, Session session) {
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
 
             if (id != session.session_id) {
-=======
-        public IHttpActionResult PutSession(string id, Session session)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != session.session_id)
-            {
->>>>>>> master
                 return BadRequest();
             }
 
             db.Entry(session).State = EntityState.Modified;
 
-<<<<<<< HEAD
             try {
                 db.SaveChanges();
             } catch (DbUpdateConcurrencyException) {
                 if (!SessionExists(id)) {
                     return NotFound();
                 } else {
-=======
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SessionExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
->>>>>>> master
                     throw;
                 }
             }
-
             return StatusCode(HttpStatusCode.NoContent);
         }
 
         // POST: api/Sessions
         [ResponseType(typeof(Session))]
-<<<<<<< HEAD
         public IHttpActionResult PostSession(Session session) {
             if (!ModelState.IsValid) {
-=======
-        public IHttpActionResult PostSession(Session session)
-        {
-            if (!ModelState.IsValid)
-            {
->>>>>>> master
                 return BadRequest(ModelState);
             }
 
             db.Sessions.Add(session);
 
-<<<<<<< HEAD
             try {
                 db.SaveChanges();
             } catch (DbUpdateException) {
                 if (SessionExists(session.session_id)) {
                     return Conflict();
                 } else {
-=======
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (SessionExists(session.session_id))
-                {
-                    return Conflict();
-                }
-                else
-                {
->>>>>>> master
                     throw;
                 }
             }
@@ -151,17 +80,9 @@ namespace EndavaScrum.Controllers
 
         // DELETE: api/Sessions/5
         [ResponseType(typeof(Session))]
-<<<<<<< HEAD
         public IHttpActionResult DeleteSession(string id) {
             Session session = db.Sessions.Find(id);
             if (session == null) {
-=======
-        public IHttpActionResult DeleteSession(string id)
-        {
-            Session session = db.Sessions.Find(id);
-            if (session == null)
-            {
->>>>>>> master
                 return NotFound();
             }
 
@@ -171,21 +92,13 @@ namespace EndavaScrum.Controllers
             return Ok(session);
         }
 
-<<<<<<< HEAD
         protected override void Dispose(bool disposing) {
             if (disposing) {
-=======
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
->>>>>>> master
                 db.Dispose();
             }
             base.Dispose(disposing);
         }
 
-<<<<<<< HEAD
         private bool SessionExists(string id) {
             return db.Sessions.Count(e => e.session_id == id) > 0;
         }
@@ -205,11 +118,5 @@ namespace EndavaScrum.Controllers
         public IHttpActionResult GetActiveSessionsNumber() {
             return Ok(db.Sessions.Count());
         }
-=======
-        private bool SessionExists(string id)
-        {
-            return db.Sessions.Count(e => e.session_id == id) > 0;
-        }
->>>>>>> master
     }
 }
--- EndavaScrum/EndavaScrum/Models/User.cs
+++ EndavaScrum/EndavaScrum/Models/User.cs
@@ -6,8 +6,6 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-
-<<<<<<< HEAD
 namespace EndavaScrum.Models {
     using System;
     using System.Collections.Generic;
@@ -22,25 +20,6 @@ namespace EndavaScrum.Models {
         public string session_id { get; set; }
         public string username { get; set; }
 
-=======
-namespace EndavaScrum.Models
-{
-    using System;
-    using System.Collections.Generic;
-    
-    public partial class User
-    {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public User()
-        {
-            this.Messages = new HashSet<Message>();
-        }
-    
-        public int user_id { get; set; }
-        public string session_id { get; set; }
-        public string username { get; set; }
-    
->>>>>>> master
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Message> Messages { get; set; }
         public virtual Session Session { get; set; }
